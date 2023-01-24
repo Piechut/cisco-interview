67992333edac0c1dbc0cf1a9f3ae0fdc45cd6485
Tomasz Piechota <tomasz.piechota@bitpanda.com>
2023-01-24 14:05:48 +0100

    cleaning

--- src/main/java/com/job/repository/CasesRepository.java
+++ src/main/java/com/job/repository/CasesRepository.java
@@ -1,7 +1,6 @@
 package com.job.repository;
 
 import com.job.resource.Case;
-import io.micronaut.data.annotation.Join;
 import io.micronaut.data.annotation.Query;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaSpecificationExecutor;
@@ -14,12 +13,8 @@ import java.util.Optional;
 @Repository
 public interface CasesRepository extends CrudRepository<Case, Long>, JpaSpecificationExecutor<Case> {
 
-  @Join(value = "user", alias = "u_", type = Join.Type.FETCH)
-  @Join(value = "notes", alias = "n_", type = Join.Type.LEFT_FETCH)
   Optional<Case> findByCaseId(@NotNull Long aLong);
 
-  @Join(value = "user", alias = "u_", type = Join.Type.FETCH)
-  @Join(value = "notes", alias = "n_", type = Join.Type.LEFT_FETCH)
   List<Case> findByStatus(Case.Status status);
 
   @Query("""
@@ -28,8 +23,6 @@ public interface CasesRepository extends CrudRepository<Case, Long>, JpaSpecific
           WHERE c.user.userId = :userId
           AND c.status = 'OPEN'
           """)
-  @Join(value = "user", alias = "u_", type = Join.Type.FETCH)
-  @Join(value = "notes", alias = "n_", type = Join.Type.LEFT_FETCH)
   List<Case> findOpenByUserUserId(Long userId);
 
 
@@ -39,7 +32,5 @@ public interface CasesRepository extends CrudRepository<Case, Long>, JpaSpecific
           WHERE c.user.userId = :userId
           AND c.status = :status
           """)
-  @Join(value = "user", alias = "u_", type = Join.Type.FETCH)
-  @Join(value = "notes", alias = "n_", type = Join.Type.LEFT_FETCH)
   List<Case> getUsersCasesByStatus(Long userId, Case.Status status);
 }
